94dee94c2c53997c99d7f556db2aef971abb0dcd
bahoo <jon.c.culver@gmail.com>
2015-09-04 12:25:13 -0700

    Content type json.

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 import csv
 import json
 import os
@@ -11,12 +11,22 @@ app = Flask(__name__)
 @app.route("/<doc_id>/")
 def dump(doc_id):
 
-    CSV_URL     = "http://docs.google.com/feeds/download/spreadsheets/Export?key=%s&exportFormat=csv&gid=0" % doc_id
-    csv_file    = requests.get(CSV_URL).text
-    fields      = [re.sub(r'\W+', '_', field.lower()) for field in csv_file.split("\r\n")[0].split(",")]
-    reader      = csv.DictReader(csv_file.split("\r\n")[1:], fields)
+    CSV_URL         = "http://docs.google.com/feeds/download/spreadsheets/Export?key=%s&exportFormat=csv&gid=0" % doc_id
+    csv_file        = requests.get(CSV_URL).text
 
-    return json.dumps([row for row in reader])
+    fields          = [re.sub(r'\W+', '_', field.lower()) for field in csv_file.split("\r\n")[0].split(",")]
+    reader          = csv.DictReader(csv_file.split("\r\n")[1:], fields)
+    
+    response_body   = json.dumps([row for row in reader])
+
+    response        = Response(response_body)
+    response.headers['Content-type'] = 'text/json'
+
+    return response
+
+
+
+
 
 
 if __name__ == "__main__":
